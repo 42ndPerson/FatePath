a10ba725b9bc8d6fc700ea8ba67623f779711efb
42ndPerson <74685713+42ndPerson@users.noreply.github.com>
2021-10-06 21:38:01 -0600

    v0.3.9 - Fixed bugs in Simulator

--- Simulator/SimulatorTests.java
+++ Simulator/SimulatorTests.java
@@ -1,5 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class SimulatorTests {
     public static void main(String[] args) {
+        //Create window
+        JFrame frame = new JFrame();
+        frame.setTitle("Simulator");
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Canvas canvas = new Canvas();
         DrawableObject square = new DrawableObject(
             new Vector2d(50,50),
@@ -12,8 +21,10 @@ public class SimulatorTests {
             0.5,
             true
         );
-
         canvas.appendToDrawableStack(square);
-        canvas.draw();
+        
+        frame.getContentPane().add(canvas);
+
+        frame.setVisible(true);
     }
 }
