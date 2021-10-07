676715ba849b4780625ae85b0bbf8266678539aa
42ndPerson <74685713+42ndPerson@users.noreply.github.com>
2021-10-06 21:57:38 -0600

    v0.3.18 - Expiremented with Simulator

--- Simulator/SimulatorTests.java
+++ Simulator/SimulatorTests.java
@@ -26,5 +26,9 @@ public class SimulatorTests {
         frame.getContentPane().add(canvas);
 
         frame.setVisible(true);
+
+        for (double i = 0; i < 20; i += 0.005) {
+            canvas.getDrawableStack()[0].setRotation(i);
+        }
     }
 }
