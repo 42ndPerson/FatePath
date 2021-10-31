856f45a6b28c2ef38f6927fc737ba3e2172d4df8
42ndPerson <74685713+42ndPerson@users.noreply.github.com>
2021-10-31 15:08:10 -0600

    v0.3.51 - Changes to Simulator

--- Simulator/SimulatorTests.java
+++ Simulator/SimulatorTests.java
@@ -29,7 +29,7 @@ public class SimulatorTests {
 
         frame.setVisible(true);
 
-        for (double i = 0; i < 300000; i += 0.01) {
+        for (double i = 0; i < 300000; i += 0.001) {
             canvas.getDrawableStack()[0].setRotation(i);
 
             frame.repaint();
